8f49d8b6393163ad8cdde1287957f26f4e40505e
naing.yoon <naing.yoon@samsung.com>
2025-07-23 12:50:06 +0900

    [Feature] p27

--- BaseballKATA/baseball_test.cpp
+++ BaseballKATA/baseball_test.cpp
@@ -4,7 +4,10 @@
 TEST(BaseballGame, TryGameTest) {
 	EXPECT_EQ(1, 1);
 }
-
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+}
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
